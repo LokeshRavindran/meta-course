39537f1dc3b6c9b95c932b31dba54a056a8a5862
Lokesh Ravindran <lokeshravindran3003@gmail.com>
2024-09-06 10:26:56 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import React from "react";
+import Home from "./routes/Home";
+import Reservations from "./routes/Reservations";
+
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/reservations" element = {<Reservations />}/>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
